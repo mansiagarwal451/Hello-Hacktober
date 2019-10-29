5c921f51bdc2f78e7f479c495ddfb1ab8b721bfd
Mansi Agarwal <mansiagarwal451@gmail.com>
2019-10-29 19:49:06 +0530

    Duplicate
    Program for finding duplicates in an array

---
+++ duplicate.cpp
@@ -0,0 +1,19 @@
+int i,n,temp;
+temp=0;
+n=A.size();
+
+map<int,int> M;
+
+for(i=0;i<n;i++)
+{
+    M[A[i]]=0;
+}
+
+for(i=0;i<n;i++)
+{
+    if(M[A[i]]==1)
+    {
+        return A[i];
+    }
+    M[A[i]]=1;
+}
